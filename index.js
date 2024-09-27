6d47689fbadc3382ebeacd02a39cb3943427b6b6
KING-HANSA <hansadewmina2008@gmail.com>
2024-09-27 21:30:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ fetchLatestBaileysVersion,
 Browsers
 } = require('@whiskeysockets/baileys')
 
+const l = console.log
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
 const P = require('pino')

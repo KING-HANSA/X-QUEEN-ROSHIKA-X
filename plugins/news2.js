e2d0cd9b5f1eefa0e5ea2ac9dc133a9e590fb43e
KING-HANSA <hansadewmina2008@gmail.com>
2024-10-31 17:34:15 +0530

    Create news2.js

---
+++ plugins/news2.js
@@ -0,0 +1,77 @@
+const config = require('../config')
+const { cmd } = require('../command')
+const axios = require('axios')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-news-apis.vercel.app/api' // API LINK ( DO NOT CHANGE THIS!! )
+
+
+// ================================LANKADEEPA NEWS========================================
+
+cmd({
+    pattern: "lankadeepanews",
+    alias: ["lankadeepa","news4"],
+    react: "🕵️‍♂️",
+    desc: "",
+    category: "news",
+    use: '.lankadeepanews',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply }) => {
+try{
+
+const news = await fetchJson(${apilink}/lankadeepa)
+  
+const msg = `
+           🕵️‍♂️ LANKADEEPA NEWS 🕵️‍♂️
+
+       
+* Title - ${news.result.title}
+
+* News - ${news.result.desc}
+
+* Date - ${news.result.date}
+
+* Link - ${news.result.url}`
+
+
+await conn.sendMessage( from, { image: { url: news.result.image || '' }, caption: msg }, { quoted: mek })
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
+
+// ================================BBC NEWS========================================
+
+cmd({
+    pattern: "bbcnews",
+    alias: ["bbc","news5"],
+    react: "⛩",
+    desc: "",
+    category: "news",
+    use: '.bbcnews',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply }) => {
+try{
+
+const news = await fetchJson(${apilink}/bbc)
+  
+const msg = `
+           ⛩ BBC NEWS ⛩
+
+       
+* Title - ${news.result.title}
+
+* News - ${news.result.desc}
+
+* Link - ${news.result.url} `
+
+
+await conn.sendMessage( from, { image: { url: news.result.image || '' }, caption: msg }, { quoted: mek })
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})

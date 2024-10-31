3df1154a8e5dcca277b0f16dbcab2ea9cb5e44a0
KING-HANSA <hansadewmina2008@gmail.com>
2024-10-31 17:29:03 +0530

    Create xviddl.js

---
+++ plugins/xviddl.js
@@ -0,0 +1,55 @@
+const { cmd } = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
+
+
+
+cmd({
+    pattern: "xvideo",
+    alias: ["xvdl","xvdown"],
+    react: "🔞",
+    desc: "Download xvideo.com porn video",
+    category: "download",
+    use: '.xvideo < text >',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply, q }) => {
+try{
+
+  if(!q) return await reply("Please give me few word !")
+    
+const xv_list = await fetchJson(${apilink}/search/xvideo?q=${q})
+if(xv_list.result.length < 0) return await reply("Not results found !")
+
+const xv_info = await fetchJson(${apilink}/download/xvideo?url=${xv_list.result[0].url})
+    
+  // FIRST VIDEO
+  
+const msg = `
+           🔞 XVIDEO DOWNLOADER 🔞
+
+       
+* Title - ${xv_info.result.title}
+
+* Views - ${xv_info.result.views}
+
+* Like - ${xv_info.result.like}
+
+* Deslike - ${xv_info.result.deslike}
+
+* Size - ${xv_info.result.size}
+`
+
+
+await conn.sendMessage( from, { image: { url: xv_info.result.image || '' }, caption: msg }, { quoted: mek })
+
+// SEND VIDEO
+await conn.sendMessage(from, { document: { url: xv_info.result.dl_link }, mimetype: "video/mp4", fileName: xv_info.result.title, caption: xv_info.result.title }, { quoted: mek });
+
+
+} catch (error) {
+console.log(error)
+reply(error)
+}
+})
